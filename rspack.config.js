45c8a9d46be2a573b570475fe534378a10d17972
Zack Chapple <zackarychapple30@gmail.com>
2024-04-15 19:20:21 -0400

    removing zephyr

--- rspack.config.js
+++ rspack.config.js
@@ -1,6 +1,9 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
-const {withZephyr} = require('zephyr-webpack-plugin')
+// const {withZephyr} = require('zephyr-webpack-plugin')
 
-module.exports = composePlugins(withNx(), withReact(), withZephyr(),(config) => {
+module.exports = composePlugins(withNx(),
+  withReact(),
+  // withZephyr(),
+  (config) => {
   return config;
 });
